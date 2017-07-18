6e5986fc0fe109ad35cc275068ed5333b32780de
alimoon <alimoon@qq.com>
2017-07-18 16:39:51 +0800

    活动详情中增加返回值

--- pages/particulars/particulars.js
+++ pages/particulars/particulars.js
@@ -12,6 +12,61 @@ Page({
   data: {
     id: ''
   },
+  
+  /*读取PDF文档 */
+  readCompletepdf: function () {
+    function showToastText(message) {
+      $wuxToast.show({
+        type: 'text',
+        timer: 1500,
+        color: '#fff',
+        text: message,
+        success: () => console.log(message)
+      })
+    }
+    wx.showLoading({
+      title: '下载中'
+    })
+    wx.downloadFile({
+      url: this.data.newsdetaillist.pdfFilePath,
+      success: function (res) {
+        wx.hideLoading()
+        var filePath = res.tempFilePath
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log('打开文档成功')
+          },
+          fail: function (res) {
+            console.log('打开文档失败')
+            wx.showToast({
+              title: '打开文档失败'
+            })
+          }
+        })
+      },
+      fail: function (res) {
+        console.log(res)
+        wx.hideLoading()
+        console.log('下载文档失败')
+        if (res.errMsg.includes("downloadFile:fail exceed max file size")) {
+          showToastText('此报告已超过微信下载限制，建议您通过官网进行浏览查看');
+        } else {
+          showToastText('下载文档失败');
+        }
+      }
+    })
+  },
+  showToastText(message) {
+    $wuxToast.show({
+      type: 'text',
+      timer: 1500,
+      color: '#fff',
+      text: message,
+      success: () => console.log(message)
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
