c00dd6fbfcf1b8eb8487ceeb4ace9fb9678b572d
yjh1587 <yjh1587@aliyun.com>
2017-07-19 13:10:35 +0800

    新增专业沉淀页面，有bug（不能上拉太快）

--- pages/express/express.js
+++ pages/express/express.js
@@ -95,7 +95,7 @@ Page({
   },
   loadmoreData: function () {
     let page = this.data.parameters.page
-    console.log(parm);
+    // console.log(parm);
     page += 1
     // console.log(page);
     let dic = this.data.parameters
