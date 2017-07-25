5bfbb205504fbf60afcb20afc6cb49a65ab46399
gitstf <shen_taifeng@126.com>
2017-07-25 10:58:04 +0800

    major
    major

--- pages/major/major.js
+++ pages/major/major.js
@@ -1,264 +1,104 @@
+// communication.js
+var imageUtil = require('../../utils/util.js');
+var TopBanner = require('../../DIYComponents/topbanner')
+var CCRequest = require('../../utils/CCRequest');
+var WxParse = require('../../wxParse/wxParse.js');
 var app = getApp()
-
-let requesturl = app.globalData.host + 'prolist'
-let classifyUrl = app.globalData.host + 'proclass'
-let contentUrl = app.globalData.host + 'procontent'
+var dataSet = {}
 Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-
+    communicationList: [],
+    imageWidth: 0,
+    parameters: {},
     nzopen: false,
-    nzshow: false,
+    nzshow: true,
     /// 筛选点击的 button
     shownavindex: -1,
     filterindex: -1,
-    src: '',
-    context:'',
-    typename:'',
-    mode:'scaleToFill',//图片显示mode
     /// 黑色背景
     isfull: false,
-    content: '',
-
+    content: [],
+    firstDesc: '',
+    typeName: '反垄断',
+    strcontext: '反垄断内容',
     // 假数据
-    filterArray: ["专业领域"],
-
-    reportsList: [],
-    parameters: {},
-    page: 1,
-
-    typeList: [],
-   
-    inputShowed: false,
-    // 搜索的word
-    inputVal: ""
-
+    filterArray: ["反垄断"],
+    areaList: [],
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    // 如果是网络请求或是别的方法传出的值调用“this”,不能直接用，需要用变量接收“this” 再使用(如mine.js中的用法)
-    this.TopImgRequest()
-    this.reportListRquest({ "proclassid": this.data.proclassid, "page": this.data.page  })
-    this.classifyRquest()
-    this.ContextRequest()
-
-  },
-  showInput: function (e) {
-    console.log("showInput", e)
-    this.setData({
-      inputShowed: true
-    });
-  },
-  hideInput: function (e) {
-    console.log("hideInput", e)
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-
+    var that = this
+    CCRequest.getPicUrl(5, function success(picUrl) {
+      console.log('交流活动的URL' + picUrl)
+      dataSet.src = picUrl
+      console.info(dataSet)
+      TopBanner.TopBanner('dataSet', dataSet, that)
+    }, function fail(data) {
+      console.log(data)
+    })
     let dic = this.data.parameters
-    console.log(dic)
-    delete (dic.keyword)
-    console.log(dic)
     dic.page = 1
-    dic.proclassid=1
-    this.reportListRquest(dic)
-  },
-  clearInput: function (e) {
-    console.log("clearInput", e)
-    this.setData({
-      inputVal: ""
-    });
-  },
-  // 暂时不需要热更新
-  inputTyping: function (e) {
-    console.log("inputTyping", e)
-    /*
-    this.setData({
-        inputVal: e.detail.value
-    });
-    */
+    this.getcommunicationslistRequest(dic)
+    this.areaListRequest()
+    this.prepareContentData()
   },
 
-  searchConfirm: function (e) {
-    console.log("searchConfirm", e)
-    this.setData({
-      inputVal: e.detail.value
-    });
- 
-    if (e.detail.value.length > 0) {
-      // 搜索长度大于0 时， 进行搜索
-      var that = this
-      this.searchData(e.detail.value, function (data) {
-        that.setData({
-          reportsList: data
-        })
+  
+  areaListRequest: function () {
+    var that = this
+    CCRequest.ccRequest('proclass', {}, function success(data) {
+      that.setData({
+        areaList: data
       })
+    }, function fail(data) {
+    })
+  },
+  
+
+  // 页面顶部图片大小绑定
+  imageLoad: function (e) {
+    console.log('图片大小')
+    var imageSize = imageUtil.imageUtil(e)
+    dataSet.size = {
+      width: imageSize.imageWidth - 15,
+      height: imageSize.imageHeight
     }
+    TopBanner.TopBanner('dataSet', dataSet, this)
+    this.setData({
+      imageWidth: imageSize.imageWidth - 15,
+    })
   },
-  TopImgRequest: function () {
+
+  /**
+   * 请求专业模块列表内容
+   */
+  getcommunicationslistRequest: function (params) {
     var that = this
-    wx.request({
-      url: app.globalData.host + 'getpic',
-      data: { "type": 5 },
-      method: 'POST',
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        // success
+    this.setData({
+      parameters: params
+    })
+    CCRequest.ccRequest('prolist', params,
+      function success(data) {
+        let arr = that.data.communicationList
+        arr = arr.concat(data)
         that.setData({
-          src: res.data.data.myPicPath
+          communicationList: arr
         })
-      },
-      fail: function (error) {
-        // fail
-        console.log(error)
-      },
-      complete: function () {
-        // complete
-      }
-    })
+        console.info('专业模块列表')
+        console.log(arr)
+      }, function fail(data) {
+      })
   },
-  ContextRequest: function () {
-    var that = this
-    wx.request({
-      url: app.globalData.host + 'procontent',
-      data: { "proclassid": 2},
-      method: 'POST',
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        // success
-        that.setData({
-          content: res.data.data.MContent
-        })
-      },
-      fail: function (error) {
-        // fail
-        console.log(error)
-      },
-      complete: function () {
-        // complete
-      }
-    })
+  lower: function () {
+    this.loadmoreData()
+    console.log('scroll bottom action')
   },
-  searchData: function (keyword, success) {
-    var that = this
-    let dic = this.data.parameters
-    dic.page = 1
-    dic.keyword = keyword
-
-    wx.showLoading({
-      title: '加载中'
-    })
-    wx.request({
-      url: requesturl,
-      data: dic,
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }, // 设置请求的 header
-      success: function (res) {
-        // success
-        console.log(res)
-        wx.hideLoading()
-        if (res.data.status == 0) {
-          that.setData({
-            parameters: dic
-          })
-          if (success) {
-            success(res.data.data)
-          }
-        } else {
-          that.setData({
-            reportsList: []
-          })
-        }
-      },
-      fail: function (res) {
-        // fail
-        console.log(res)
-        wx.hideLoading()
-      },
-    })
-  },
-
-  // 专业模块列表
-  reportListRquest: function (parameters) {
-    var that = this
-    wx.showLoading({
-      title: '加载中',
-    })
-    wx.request({
-      url: requesturl,
-      data: parameters,
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        // success
-        wx.hideLoading()
-        console.log(res)
-        if (res.data.status == 0) {
-          that.setData({
-            reportsList: res.data.data,
-            parameters: parameters
-          })
-        }
-      },
-      fail: function (res) {
-        // fail
-        console.log(res)
-        wx.hideLoading()
-      },
-      complete: function (res) {
-        // complete
-      }
-    })
-  },
-
-
-
-  classifyRquest: function () {
-    var that = this
-    wx.request({
-      url: classifyUrl,
-      data: {},
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      success: function (res) {
-        // success
-        console.log(res)
-        if (res.data.status == 0) {
-          that.setData({
-            typeList: res.data.data
-          })
-        }
-
-      },
-      fail: function (res) {
-        // fail
-        console.log(res)
-      },
-      complete: function (res) {
-        // complete
-      }
-    })
-  },
-
-  onPullDownRefresh: function () {
-    // 页面相关事件处理函数--监听用户下拉动作
-    // this.reportsList()
-  },
-  onReachBottom: function () {
-    // 页面上拉触底事件的处理函数
-    if (this.data.nzshow) {
-      this.loadmoreData()
-    }
-
-  },
- 
 
   loadmoreData: function () {
     let page = this.data.parameters.page
@@ -266,59 +106,19 @@ Page({
     let dic = this.data.parameters
     dic.page = page
     var that = this
-    wx.showLoading({
-      title: '加载中',
-    })
-    console.log(dic)
-    wx.request({
-      url: requesturl,
-      data: dic,
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        // success
-        console.log(res)
-        wx.hideLoading()
-        if (res.data.status == 0) {
-          let arr = that.data.reportsList.concat(res.data.data)
-          console.log(arr)
-          that.setData({
-            parameters: dic,
-            reportsList: arr
-          })
-        }
-      },
-      fail: function (res) {
-        // fail
-        wx.hideLoading()
-      },
-      complete: function (res) {
-        // complete
-      }
-    })
+    console.log('上拉加载')
+    // console.info(dic)
+    this.getcommunicationslistRequest(dic)
   },
-  // 跳至报告详情页
-  toReportDetail: function (e) {
-    let index = e.currentTarget.dataset.detailid
-    let item = this.data.reportsList[index]
-    let ID = item.ID
-    wx.navigateTo({
-      url: './majordetail/majordetail?id=' + ID
-    })
-  },
-
   filterItemAction: function (e) {
-    let array = ["专业领域"]
-    let keys = ["typeID"]
+    let array = ["反垄断"]
+    let keys = ["proclassid"]
     var parameters = this.data.parameters
     let keyindex = this.data.shownavindex
     let arr = this.data.content[this.data.shownavindex]
+    console.log(arr)
     var filterArray = this.data.filterArray
     let index = e.currentTarget.dataset.filter
-    let strtypename=''
     if (index == 0) {
       //选择全部, 清楚筛选条件
       filterArray[keyindex] = array[keyindex]
@@ -329,29 +129,32 @@ Page({
         filterArray: filterArray,
         parameters: parameters
       })
+      this.setData({
+        typeName:'反垄断'
+      })
     } else {
+      console.log(index)
       let item = arr[index]
-      strtypename = item
+      console.log(item)
+      this.setData({
+        typeName: item
+      })
       filterArray[keyindex] = item
       let key = keys[keyindex]
-      if (this.data.shownavindex == 1) {
-        parameters[key] = this.data.typeList[index - 1].ID
+       if (this.data.shownavindex == 0) {
+        parameters[key] = this.data.areaList[index - 1].ID
       }
       this.setData({
         filterindex: index,
         filterArray: filterArray,
-        typename: strtypename,
         parameters: parameters
       })
     }
     this.hidebg()
 
-
     console.log(parameters)
-
     this.filterRequest()
   },
-
   filterRequest: function () {
     var that = this
     wx.showLoading({
@@ -359,43 +162,14 @@ Page({
     })
     let parameters = this.data.parameters
     parameters.page = 1
-    console.log(parameters)
-    wx.request({
-      url: requesturl,
-      data: parameters,
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      success: function (res) {
-        // success
-        wx.hideLoading()
-        console.log(res)
-        if (res.data.status == 0) {
-          that.setData({
-            parameters: parameters,
-            reportsList: res.data.data
-          })
-        } else {
-          that.setData({
-            parameters: parameters,
-            reportsList: []
-          })
-        }
-
-      },
-      fail: function (res) {
-        // fail
-        wx.hideLoading()
-      },
-      complete: function (res) {
-        // complete
-      }
-    })
+    CCRequest.ccRequest('prolist', parameters, function success(data) {
+      that.setData({
+        parameters: parameters,
+        activityList: data
+      })
+    }, function fail(data) { })
+    this.prepareContentData()
   },
-
-
   filterAction: function (view) {
     console.log(view)
     let index = view.currentTarget.dataset.hi
@@ -407,6 +181,7 @@ Page({
         nzshow: false,
         content: this.getContent(index),
       })
+      console.log(this.data.content)
     } else {
       let navindex = this.data.shownavindex
       if (index == navindex) {
@@ -417,6 +192,8 @@ Page({
           nzshow: true,
           content: [],
         })
+     
+        console.log(this.data.content)
       } else {
         this.setData({
           shownavindex: index,
@@ -425,23 +202,10 @@ Page({
           nzshow: false,
           content: this.getContent(index),
         })
+      
+        console.log(this.data.content)
       }
     }
-  },
-
-  getContent: function (index) {
-    // ["专业领域"]
-    var content = []
-    if (index == 0) {
-      let arr = this.data.typeList
-      var arr1 = []
-      for (var i = 0; i < arr.length; i++) {
-        var element = arr[i];
-        arr1.push(element.Desc)
-      }
-      content[index] = arr1
-    } 
-    return content
   },
 
   hidebg: function (e) {
@@ -453,27 +217,104 @@ Page({
       nzshow: true
     })
   },
+  getContent: function (index) {
+    // ["专业领域"]
+    var content = []
+   if (index == 0) {// 领域
+      let arr = this.data.areaList
+      var arr1 = []
+      for (var i = 0; i < arr.length; i++) {
+        var element = arr[i];
+        arr1.push(element.Desc)
+      }
+      content[index] = arr1
+    } 
+    console.log('content')
+    console.log(content)
+    return content
+  },
+  prepareContentData: function () {
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
 
-  
+    wx.request({
+      url: app.globalData.host + 'procontent',
+      data: this.data.parameters,
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }, // 设置请求的 header
+      success: function (res) {
+        // success
+        console.log('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+        if (res.data.status == 0) {
+        console.info(res.data.data)
+   
+          wx.hideLoading()
+          console.log(WxParse)
+          console.log(res.data.data.MContent)
+          console.log('b1')
+          var article = '<div>' + res.data.data.MContent + '</div>';
+         
+          WxParse.wxParse('article', 'html', article, that, 5);
+        }
+      },
+      fail: function (res) {
+        // fail
+      },
+      complete: function (res) {
+        // complete
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
   onReady: function () {
-    // 页面渲染完成
+
   },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
   onShow: function () {
-    // 页面显示
+
   },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
   onHide: function () {
-    // 页面隐藏
+
   },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
   onUnload: function () {
-    // 页面关闭
+
   },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
   onShareAppMessage: function () {
-    // 用户点击右上角分享
-    return {
-      // title: 'title', // 分享标题
-      // desc: 'desc', // 分享描述
-      path: 'pages/report/report' // 分享路径
-    }
+
   }
 })
-
