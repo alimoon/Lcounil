530718874e8c394793037706c83223ca54b86de0
chenweihong <15094846015@163.com>
2017-08-25 11:00:06 +0800

    法务薪酬

--- pages/salary/salary.js
+++ pages/salary/salary.js
@@ -5,9 +5,12 @@ var WxParse = require('../../wxParse/wxParse.js');
 let requestUrl = app.globalData.host + 'getpic';
 var CCRequest = require('../../utils/CCRequest');
 var imageUtil = require('../../utils/util.js');
+import { $wuxToast } from '../../components/wux'
+import { $wuxDialog } from '../../components/wux' 
 Page({
   data: {
     item: '',
+    ID: '',
     imagewidth: 0,//缩放后的宽 
     imageheight: 0,//缩放后的高
     picPath: '',
@@ -58,7 +61,10 @@ Page({
         if (res.data.status == 0) {
           wx.hideLoading()
           console.log(WxParse)
-          console.log(res.data.data.content)
+          console.log(res.data.data)
+          that.setData({
+            item: res.data.data
+          })
           var article = '<div>' + res.data.data.Desc + '</div>';
           /**
           * WxParse.wxParse(bindName , type, data, target,imagePadding)
@@ -79,6 +85,84 @@ Page({
       }
     })
   },
+  // 跳转到报告页面再查看PDF（不可取，弃用）
+  toRead: function () {
+    wx.navigateTo({
+        url: '../particulars/particulars?id=' + this.data.item.ID
+    })
+  },
+
+  /*直接读取PDF文档 */
+  readCompletepdf: function () {
+    // 先判断是否登录，未登录需先登录，已登录则从本地获取用户信息
+    let islogin = wx.getStorageSync('isLogin')
+    if (islogin == false) {//未登录
+        $wuxDialog.confirm({
+            title: '登录后才能查看', 
+            content: '您确定要登录吗？',
+            onConfirm(e) {
+              // alert(content)
+              console.log('用户点击确定')
+              console.log(e)
+              wx.navigateTo({
+                  url: '../login/login'
+              })
+            },
+        })
+        return
+    }
+    function showToastText(message) {
+      $wuxToast.show({
+        type: 'text',
+        timer: 1500,
+        color: '#fff',
+        text: message,
+        success: () => console.log(message)
+      })
+    }
+    wx.showLoading({
+      title: '下载中'
+    })
+    wx.downloadFile({
+      url: this.data.item.FilePath,
+      success: function (res) {
+        wx.hideLoading()
+        var filePath = res.tempFilePath
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log('打开文档成功')
+          },
+          fail: function (res) {
+            console.log('打开文档失败')
+            wx.showToast({
+              title: '打开文档失败'
+            })
+          }
+        })
+      },
+      fail: function (res) {
+        console.log(res)
+        wx.hideLoading()
+        console.log('下载文档失败')
+        if (res.errMsg.includes("downloadFile:fail exceed max file size")) {
+          showToastText('此报告已超过微信下载限制，建议您通过官网进行浏览查看');
+        } else {
+          showToastText('下载文档失败');
+        }
+      }
+    })
+  },
+  showToastText(message) {
+    $wuxToast.show({
+      type: 'text',
+      timer: 1500,
+      color: '#fff',
+      text: message,
+      success: () => console.log(message)
+    })
+  },
+
   onReady: function () {
     // 页面渲染完成
   },
