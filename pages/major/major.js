72066841f8abb833ec9e5cbbc231b4932f666493
yjh1587 <yaojiehua@yaojiehuadeMacBook-Pro.local>
2017-09-07 08:52:07 +0800

    专业模块右对齐和下拉选项新增

--- pages/major/major.js
+++ pages/major/major.js
@@ -25,7 +25,7 @@ Page({
     typeName: '反垄断',
     strcontext: '反垄断内容',
     // 假数据
-    filterArray: ["反垄断"],
+    filterArray: ["《19个专业模块》"],
     areaList: [],
     inputShowed: false,
     // 搜索的word
